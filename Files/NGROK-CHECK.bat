b63e9bc27e6be40da520bdf752d766cac1ce3079
Neon Dev <83107374+NeonDev01@users.noreply.github.com>
2023-04-15 10:41:24 +0530

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -2,4 +2,4 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: runneradmin
-echo Pass: P@ssw0rd
+echo Pass: P@ssw0rd!
